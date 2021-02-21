f1e067d7fab936e1ccc45d15ff7b6801d78fbe4c
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2021-02-21 10:41:09 -0500

    add GMaps functions

--- seeing-sismic-event/Form1.Designer.cs
+++ seeing-sismic-event/Form1.Designer.cs
@@ -30,6 +30,15 @@ namespace seeing_sismic_event
         private void InitializeComponent()
         {
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gMapControl1
@@ -40,9 +49,9 @@ namespace seeing_sismic_event
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(524, 85);
+            this.gMapControl1.Location = new System.Drawing.Point(239, 12);
             this.gMapControl1.MarkersEnabled = true;
-            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MaxZoom = 18;
             this.gMapControl1.MinZoom = 2;
             this.gMapControl1.MouseWheelZoomEnabled = true;
             this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
@@ -54,25 +63,127 @@ namespace seeing_sismic_event
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(150, 150);
+            this.gMapControl1.Size = new System.Drawing.Size(605, 389);
             this.gMapControl1.TabIndex = 0;
-            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.Zoom = 13D;
+            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.Location = new System.Drawing.Point(75, 288);
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.Size = new System.Drawing.Size(135, 20);
+            this.txtLatitude.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Latitude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Longitude";
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.Location = new System.Drawing.Point(75, 337);
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.Size = new System.Drawing.Size(135, 20);
+            this.txtLongitude.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Type";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Marker",
+            "Polygon"});
+            this.comboBox.Location = new System.Drawing.Point(75, 385);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(135, 21);
+            this.comboBox.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 439);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(135, 439);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 8;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 226);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(856, 517);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLongitude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLatitude);
             this.Controls.Add(this.gMapControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.TextBox txtLatitude;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
--- seeing-sismic-event/Form1.cs
+++ seeing-sismic-event/Form1.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
 
 namespace seeing_sismic_event
 {
-    public partial class Form1 : Form
+    public partial class Form1: Form
     {
+        //attributes
+        private List<PointLatLng> points;
+        private List<PointLatLng> shapes;
+        GMapOverlay markers = new GMapOverlay("markers");
+        GMapOverlay polygons = new GMapOverlay("polygond");
+
         public Form1()
         {
             InitializeComponent();
+            points = new List<PointLatLng>();
+            shapes = new List<PointLatLng>();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            gMapControl1.Position = new PointLatLng(3.42158, -76.5205);
+            gMapControl1.Overlays.Add(markers);
+            gMapControl1.Overlays.Add(polygons);
+        }
+
+        private void setMarkers()
+        {
+            foreach(PointLatLng p in points)
+            {
+                GMapMarker marker = new GMarkerGoogle(p, GMarkerGoogleType.red_dot);
+                markers.Markers.Add(marker);
+            }
+        }
+
+        private void setPolygons()
+        {
+            GMapPolygon polygon = new GMapPolygon(shapes, "polygon");
+            polygon.Fill = new SolidBrush(Color.FromArgb(50, Color.Red));
+            polygon.Stroke = new Pen(Color.Red, 1);
+            polygons.Polygons.Add(polygon);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double latitude = double.Parse(txtLatitude.Text);
+            txtLatitude.Text = "";
+            double longitude = double.Parse(txtLongitude.Text);
+            txtLongitude.Text = "";
+            PointLatLng p = new PointLatLng(latitude, longitude);
+            if (comboBox.Text == "Marker")
+            {
+                points.Add(p);
+            }else if (comboBox.Text == "Polygon")
+            {
+                shapes.Add(p);
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (comboBox.Text == "Marker")
+            {
+                setMarkers();
+                points.Clear();
+            }else if (comboBox.Text == "Polygon")
+            {
+                setPolygons();
+                shapes.Clear();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            markers.Clear();
+            polygons.Clear();
+            shapes.Clear();
         }
     }
 }
